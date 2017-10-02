c4d32377ddb90fbe66fa31727b780f8f3d214770
AlejandroGarcia92 <agarciamoreno@fundaciocim.org>
2017-10-02 14:31:34 +0200

    new genieArduino lib

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -27,8 +27,8 @@
 //This is the version declaration for Sigma, v followed by '-' first indicate the hardware, it must have 2 ditgits. Then the '-' and then the firmware, it has to have 3 digits separets by '.'. -> This is useful to
 //get the hw and fw version to Cura-BCN3D and update the new firmware
 
-#define VERSION_STRING  "02-1.2.6RC"
-#define BUILD_DATE  "|M09.26"
+#define VERSION_STRING  "01-1.2.6RC"
+#define BUILD_DATE  "|M09.30"
 #define VERSION_NUMBER  126
 //#define BUILD_DATE  " "
 #define UI_SerialID  "At Bottom Sticker"
@@ -113,7 +113,7 @@
 #endif
 
 #ifndef BCN3D_PRINTER_SETUP
-	#define BCN3D_PRINTER_SETUP BCN3D_SIGMA_PRINTER_DEVMODE_1
+	#define BCN3D_PRINTER_SETUP BCN3D_SIGMA_PRINTER_DEFAULT
 #endif
 
 #ifndef BCN3D_SCREEN_VERSION_SETUP
