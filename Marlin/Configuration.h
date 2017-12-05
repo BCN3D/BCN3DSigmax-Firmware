87fdf2d6c3e75921c7cf30aeef1b1e67fda0a900
AlejandroGarcia92 <agarciamoreno@fundaciocim.org>
2017-12-05 10:25:48 +0100

    12.05

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -24,11 +24,11 @@
 // For a Delta printer replace the configuration files with the files in the
 // example_configurations/SCARA directory.
 //
-//This is the version declaration for Sigma, v followed by '-' first indicate the hardware, it must have 2 ditgits. Then the '-' and then the firmware, it has to have 3 digits separets by '.'. -> This is useful to
+//This is the version declaration for Sigmax, v followed by '-' first indicate the hardware, it must have 2 ditgits. Then the '-' and then the firmware, it has to have 3 digits separets by '.'. -> This is useful to
 //get the hw and fw version to Cura-BCN3D and update the new firmware
 
 #define VERSION_STRING  "02-1.2.9RC"
-#define BUILD_DATE  "|M11.24"
+#define BUILD_DATE  "|M12.05"
 #define VERSION_NUMBER  129
 //#define BUILD_DATE  " "
 #define UI_SerialID  "At Bottom Sticker"
